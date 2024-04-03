8cc351ec1f492fecdebdbebd7751f452dfc94c8c
lezernack <luks.ezck@gmail.com>
2024-04-02 18:02:40 -0600

    added an api to mongodb

--- node-express-course-main/03-task-manager/starter/db/connect.js
+++ node-express-course-main/03-task-manager/starter/db/connect.js
@@ -1,2 +1,14 @@
+const mongoose = require("mongoose");
+
 const connectionString =
-  "mongodb+srv://luksezck:<password>@nodeexpressprojects.yvsootq.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProjects";
+  "mongodb+srv://luksezck:flash@nodeexpressprojects.yvsootq.mongodb.net/";
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("CONNECTED TO THE Db..."))
+  .catch((err) => console.log(err));
