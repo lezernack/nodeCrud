abeb3e482875c7acabe824a871d940d43d6dcbcc
lezernack <luks.ezck@gmail.com>
2024-04-03 16:53:42 -0600

    made folder and changed code

--- node-express-course-main/03-task-manager/starter/app.js
+++ node-express-course-main/03-task-manager/starter/app.js
@@ -3,6 +3,7 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect").default;
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
 // middleware
 app.use(express.static("./public"));
 app.use(express.json());
@@ -11,11 +12,7 @@ app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
 
-// app.get('/api/v1/tasks')        -get all the tasks
-// app.post('/api/v1/tasks')       -create a new task
-// app.get('/api/v1/tasks/:id')    -get single task
-// app.patch('/api/v1/tasks/:id')  -update task
-// app.delete('/api/v1/tasks/:id') -delete task
+app.use(notFound);
 
 const port = 3000;
 
---
+++ node-express-course-main/03-task-manager/starter/middleware/error-handler.js
@@ -0,0 +1,1 @@
+const errorHandlerMiddlware = (err, req, res, next) => {};
